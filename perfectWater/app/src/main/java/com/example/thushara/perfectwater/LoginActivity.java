50599121fdbfef425530aeeb9b20ceb52eafc7d9
raghavrajmittal <raj.raghav@gmail.com>
2017-02-12 22:54:50 -0500

    fixed user credentials

--- perfectWater/app/src/main/java/com/example/thushara/perfectwater/LoginActivity.java
+++ perfectWater/app/src/main/java/com/example/thushara/perfectwater/LoginActivity.java
@@ -49,7 +49,7 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
      * TODO: remove after connecting to a real authentication system.
      */
     private static final String[] DUMMY_CREDENTIALS = new String[]{
-            "raghavmittal@gatech.edu:banana", "bar@example.com:world"
+            "user:pass", "bar@example.com:world"
     };
     /**
      * Keep track of the login task to ensure we can cancel it if requested.
@@ -192,7 +192,7 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
 
     private boolean isEmailValid(String email) {
         //TODO: Replace this with your own logic
-        return email.contains("@");
+        return email.length() > 0;
     }
 
     private boolean isPasswordValid(String password) {
