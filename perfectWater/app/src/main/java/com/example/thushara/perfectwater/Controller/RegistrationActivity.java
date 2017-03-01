7a95f8a7ad8c2a26ec8f7718a3d429edcfa63c6c
thushara@gatech.edu <thushara@gatech.edu>
2017-03-01 02:00:50 -0500

    storing the values in the registration to be accessed later, edited one of the strings xml that was causing a build failure, removed two fo the title_activity_registration that was causing a build failure.
    :build is successful

--- perfectWater/app/src/main/java/com/example/thushara/perfectwater/Controller/RegistrationActivity.java
+++ perfectWater/app/src/main/java/com/example/thushara/perfectwater/Controller/RegistrationActivity.java
@@ -2,26 +2,28 @@ package com.example.thushara.perfectwater.Controller;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.support.design.widget.TextInputLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.ArrayAdapter;
-import android.widget.AutoCompleteTextView;
 import android.widget.Button;
-import android.widget.EditText;
 import android.widget.Spinner;
-
+import android.widget.TextView;
+import android.widget.EditText;
 import com.example.thushara.perfectwater.R;
+
+import java.util.ArrayList;
 
 import model.UserType;
 
 public class RegistrationActivity extends AppCompatActivity {
 
-    private Spinner userTypeSpinner;
-
+    private Spinner userType;
+    private TextView name ;
+    private TextView username;
+    private TextView password;
+    private ArrayList records;
+    private String[] info = {name.toString(), username.toString(), password.toString(), records.toString()};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,11 +32,22 @@ public class RegistrationActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        /**
+         * Grab the dialog widgets so we can get info for later
+         */
+
+        name = (EditText) findViewById(R.id.register_name);
+        username = (EditText) findViewById(R.id.register_username);
+        password = (EditText) findViewById(R.id.register_password);
+        userType = (Spinner) findViewById(R.id.usertypeSpinner);
+        records.add(info);
+
+
         //loads the spinners and sets the values in it
-        userTypeSpinner = (Spinner) findViewById(R.id.usertypeSpinner);
+        userType = (Spinner) findViewById(R.id.usertypeSpinner);
         ArrayAdapter<UserType> userTypeAdapter = new ArrayAdapter(this,android.R.layout.simple_spinner_item, UserType.values());
         userTypeAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        userTypeSpinner.setAdapter(userTypeAdapter);
+        userType.setAdapter(userTypeAdapter);
 
         Button registrationButton = (Button) findViewById(R.id.registration_button);
         registrationButton.setOnClickListener(new View.OnClickListener() {
