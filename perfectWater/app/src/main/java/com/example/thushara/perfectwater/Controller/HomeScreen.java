3ebb4db3de15e9cb8bf188e6d8eda86da13e9ffa
raghavrajmittal <raj.raghav@gmail.com>
2017-04-06 00:40:04 -0400

    FIrebase Login added and fixed

--- perfectWater/app/src/main/java/com/example/thushara/perfectwater/Controller/HomeScreen.java
+++ perfectWater/app/src/main/java/com/example/thushara/perfectwater/Controller/HomeScreen.java
@@ -2,6 +2,7 @@ package com.example.thushara.perfectwater.Controller;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
@@ -11,20 +12,55 @@ import android.widget.Button;
 
 import com.example.thushara.perfectwater.R;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.View;
+import com.firebase.ui.*;
+
+import com.example.thushara.perfectwater.R;
+import com.firebase.ui.auth.AuthUI;
+import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+
+import java.util.ArrayList;
+import java.util.List;
+
 
 /**
- * describe method
- * @param
- * @return
- * @throws
+ * Home screen activity that contains firebase login and buttons for all the functionality
  */
-
 public class HomeScreen extends AppCompatActivity {
+    private static final int RC_SIGN_IN = 0;
+    private FirebaseAuth auth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_screen);
+        List<AuthUI.IdpConfig> providers = new ArrayList<>();
+        providers.add(new AuthUI.IdpConfig.Builder(AuthUI.EMAIL_PROVIDER).build());
+        providers.add(new AuthUI.IdpConfig.Builder(AuthUI.GOOGLE_PROVIDER).build());
+        providers.add(new AuthUI.IdpConfig.Builder(AuthUI.FACEBOOK_PROVIDER).build());
+
+        auth = FirebaseAuth.getInstance();
+        if (auth.getCurrentUser() != null) {
+            //user already signed in
+            Log.d("AUTH", auth.getCurrentUser().getEmail());
+        } else {
+            startActivityForResult(AuthUI.getInstance()
+                    .createSignInIntentBuilder().setProviders(
+                            providers
+                    )
+                    .build(), RC_SIGN_IN);
+        }
+
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
@@ -41,8 +77,14 @@ public class HomeScreen extends AppCompatActivity {
         logout_button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                finish();
-                startActivity(new Intent(HomeScreen.this, LoginActivity.class));
+                AuthUI.getInstance().signOut(HomeScreen.this).addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        finish();
+                    }
+                });
+//                finish();
+//                startActivity(new Intent(HomeScreen.this, LoginActivity.class));
             }
         });
         //Edit Profile button functionality
@@ -73,6 +115,20 @@ public class HomeScreen extends AppCompatActivity {
             }
         });
 
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == RC_SIGN_IN) {
+            if(resultCode == RESULT_OK) {
+                //user logged in
+                Log.d("AUTH", auth.getCurrentUser().getEmail());
+            } else {
+                // user not authenticated
+                Log.d("AUTH", "NOT AUTHENTICATED");
+            }
+        }
     }
 
 }
