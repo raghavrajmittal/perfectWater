3d80d606d7fe9af26ca4b052f1f7e042fd67e874
Baria Manahill <bmanahill@gmail.com>
2017-04-06 12:25:34 -0400

    SQLite Database created with connection

---
+++ perfectWater/app/src/main/java/com/example/thushara/perfectwater/Controller/SQLiteConnection.java
@@ -0,0 +1,8 @@
+package com.example.thushara.perfectwater.Controller;
+
+/**
+ * Created by baria on 4/6/17.
+ */
+
+public class SQLiteConnection {
+}
---
+++ perfectWater/app/src/main/java/model/LoginSQL.java
@@ -0,0 +1,8 @@
+package model;
+
+/**
+ * Created by baria on 4/6/17.
+ */
+
+public class LoginSQL {
+}
